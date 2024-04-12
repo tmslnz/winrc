d3f7404bf389c3b2ec33b388514fa7e335c27fe6
Tommaso Lanza <t@theworkers.net>
2024-04-12 20:25:22 +0100

    pyenv

--- winrc.ps1
+++ winrc.ps1
@@ -190,6 +190,11 @@ function Install-Scoop {
     Invoke-RestMethod -Uri 'https://get.scoop.sh' | Invoke-Expression
 }
 
+function Install-Pyenv-Win {
+    Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"
+    & "./install-pyenv-win.ps1"
+    Remove-Item "./install-pyenv-win.ps1"
+}
 function Install-Scoop-Apps {
     if (-Not (Get-Command scoop -ErrorAction SilentlyContinue)) { return }
     scoop update
